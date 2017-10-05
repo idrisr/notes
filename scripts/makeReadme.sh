d6f35e6d06a67d2b0cdcb5163d75d0e87aab4202
Id Raja <iraja@nerdery.com>
2017-10-05 14:02:26 -0500

    make the script work better

--- scripts/makeReadme.sh
+++ scripts/makeReadme.sh
@@ -4,13 +4,16 @@
 # move all the mds to its own dir
 # make this a git hook
 
-dir="../"
+curdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+dir=${curdir}/../
+readme=${dir}/README.md
 files=$(find ${dir} -iname '*md' -type f | sed 's|.*/||')
 
+rm ${readme}
 for f in ${files}
 do 
     filename=$(basename "$")
     extension="${f##*.}"
     filename_noext="${f%.*}"
-    echo \* [${filename_noext}]\(\.\/${filename_noext}.md\)
+    echo \* [${filename_noext}]\(\.\/${filename_noext}.md\) >> ${readme}
 done
